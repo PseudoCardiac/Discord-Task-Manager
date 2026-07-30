b8bd872d94dc93de50c4179e2ef3b05fc24a4b7f
42 <nooyeahc1125@gmail.com>
2026-07-30 14:55:17 +0900

    JSON 초기화 커맨드 추가

--- cogs/export_json.py
+++ cogs/export_json.py
@@ -1,5 +1,6 @@
-import discord
+import discord, json
 from discord.ext.commands import Cog
+from utils import genChart
 
 
 class ExportJsonCog( Cog ):
@@ -15,3 +16,16 @@ class ExportJsonCog( Cog ):
             timetable = discord.File( f )
 
         await i.response.send_message( files = [ currentTasks, today, timetable ] )
+
+
+    @discord.app_commands.command( name = "초기화", description = "JSON 파일을 초기화한다" )
+    async def resetJson( self, i: discord.Interaction ):
+        with open( "data/current_tasks.json", 'w' ) as f:
+            json.dump( {}, f )
+
+        with open( "data/today.json", 'w' ) as f:
+            json.dump( {}, f )
+
+        genChart()
+
+        await i.response.send_message( "JSON 초기화됨" )

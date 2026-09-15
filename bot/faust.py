a53cee4f10c8a9d3ba319053a34bc5c61c951c5d
42 <nooyeahc1125@gmail.com>
2026-09-16 06:35:36 +0900

    `memorySnapshotTask`에서 `info`의 정보를 이용하도록 수정

--- bot/faust.py
+++ bot/faust.py
@@ -41,8 +41,10 @@ class Faust( Bot ):
                 for stat in top_stats[:10]:
                     statMsg += statMsg + str( stat ) + '\n'
 
-                scy = await self.fetch_user( 513676568745213953 )
-                await scy.send( statMsg )
+                await self.info.scy.send( statMsg )
+
+            else:
+                await self.info.scy.send( "메모리 모니터링 개시" )                
 
             snapshot1 = snapshot2
 

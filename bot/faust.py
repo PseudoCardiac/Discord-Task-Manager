3e7147c7b51b79a710beb22c0ac084cf2040d12c
42 <nooyeahc1125@gmail.com>
2026-09-16 06:32:42 +0900

    `on_ready`를 `setup_hook`로 대체, 메모리 누수 체크용 태스크 추가

--- bot/faust.py
+++ bot/faust.py
@@ -1,4 +1,4 @@
-import discord, os, asyncio
+import discord, os, asyncio, tracemalloc
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 from objects import Info, TaskEmbedView
@@ -6,12 +6,15 @@ from cogs import TaskManagementCog, DailyReportCog, FileManagementCog, PresenceL
 from utils import TimelineView
 
 
+tracemalloc.start()
+
+
 class Faust( Bot ):
     def __init__( self ):
         super().__init__( command_prefix = "@Faust", intents = discord.Intents.all() )
 
 
-    async def on_ready( self ):
+    async def setup_hook( self ):
         self.info = Info()
         await self.info.init( self )
         await self.add_cog( TaskManagementCog( self ) )
@@ -21,6 +24,30 @@ class Faust( Bot ):
         self.add_view( TimelineView() )
         self.add_view( TaskEmbedView() )
         # await self.tree.sync()
+        self.memory_task = asyncio.create_task( self.memorySnapshotTask() )
+
+
+    async def memorySnapshotTask( self ):
+        snapshot1 = None
+
+        while True:
+            await asyncio.sleep( 3600 )
+            snapshot2 = tracemalloc.take_snapshot()
+
+            if snapshot1:
+                top_stats = snapshot2.compare_to( snapshot1, 'lineno' )
+                statMsg = "[ 메모리 증가 Top 10 ]\n"
+
+                for stat in top_stats[:10]:
+                    statMsg += statMsg + str( stat ) + '\n'
+
+                scy = await self.fetch_user( 513676568745213953 )
+                await scy.send( statMsg )
+
+            snapshot1 = snapshot2
+
+
+    async def on_ready( self ):
         print( "파우스트 온라인." )
 
 
